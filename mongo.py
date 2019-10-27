b9e38c4e5dbb4260be4ba02ca4129a6973152862
EletronSz <janetuker2@gmail.com>
2019-10-27 16:50:52 +0200

    edited users

--- mongo.py
+++ mongo.py
@@ -8,22 +8,22 @@ db = client.Api
 cust = db['customers']
 
 #find all customers
-curs = cust.find()
-for cu in curs:
-    #pass
-    print(cu)
+# curs = cust.find()
+# for cu in curs:
+#     #pass
+#     print(cu)
 
 #find one customer
-#print(cust.find_one())
+print(cust.find({'_id', '5db5896e65d18824616570a1'}))
 
 #insert one customer
-customer = {
-    'reg_date': datetime.datetime.utcnow(),
-    'fname': "Buhle",
-    'lname': "Mbhamali",
-    'age': 23,
-    'email': "buhle@fmail.com"
-}
+# customer = {
+#     'reg_date': datetime.datetime.utcnow(),
+#     'fname': "Buhle",
+#     'lname': "Mbhamali",
+#     'age': 23,
+#     'email': "buhle@fmail.com"
+# }
 
-cust_id = cust.insert_one(customer)
+# cust_id = cust.insert_one(customer)
 #print(cust_id)
