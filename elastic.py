fe1b69180c0184e1412a520a4a99167e8f2d2b8a
EletronSz <janetuker2@gmail.com>
2019-10-28 21:36:42 +0200

    Maxis

---
+++ elastic.py
@@ -0,0 +1,8 @@
+from elasticsearch import Elasticsearch
+es = Elasticsearch()
+
+# ignore 400 cause by IndexAlreadyExistsException when creating an index
+es.indices.create(index='test-index', ignore=400)
+
+# ignore 404 and 400
+es.indices.delete(index='test-index', ignore=[400, 404])
