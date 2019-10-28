d968419dc80bce8905cbdd6e3ef7ae3dc1cd8c76
EletronSz <janetuker2@gmail.com>
2019-10-28 22:08:29 +0200

    trip

--- elastic.py
+++ elastic.py
@@ -4,5 +4,4 @@ es = Elasticsearch()
 # ignore 400 cause by IndexAlreadyExistsException when creating an index
 es.indices.create(index='test-index', ignore=400)
 
-# ignore 404 and 400
-es.indices.delete(index='test-index', ignore=[400, 404])
+es.search(index='test-index', filter_path=['hits.hits._id', 'hits.hits._type'])
